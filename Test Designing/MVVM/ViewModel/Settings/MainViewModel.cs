8d84b5643813697199b22853e00a5a2745726e98
Jay Feather2000 <thegals4321@gmail.com>
2022-10-01 23:43:16 -0500

    A

---
+++ Test Designing/MVVM/ViewModel/Settings/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_Designing.Core;
+
+namespace Test_Designing.MVVM.ViewModel.Settings
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand ThemeViewCommand { get; set; }
+
+        public ThemeViewModel ThemeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            ThemeVM = new ThemeViewModel();
+            CurrentView = ThemeVM;
+
+            ThemeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ThemeVM;
+            });
+        }
+    }
+}
